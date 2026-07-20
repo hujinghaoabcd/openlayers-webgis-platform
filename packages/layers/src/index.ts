2a770a38fdb263fd1b886304e35983e064247bf3
Jinghao Hu <731409213@qq.com>
2026-07-20 10:16:43 +0800

    feat: add raster vector and format layer factories

--- packages/layers/src/index.ts
+++ packages/layers/src/index.ts
@@ -4,4 +4,23 @@ export {
   createWmsLayer,
   createXyzLayer,
 } from './factories.js';
+export {createLayerGroup} from './group.js';
+export {createGeoTiffLayer, createImageWmsLayer, createWmtsLayer} from './raster.js';
+export {
+  createGpxLayer,
+  createKmlLayer,
+  createVectorLayer,
+  createVectorTileLayer,
+  createWktLayer,
+} from './vector.js';
 export type {NamedLayerOptions, WmsLayerOptions, XyzLayerOptions} from './factories.js';
+export type {LayerGroupOptions} from './group.js';
+export type {GeoTiffLayerOptions, ImageWmsLayerOptions, WmtsLayerOptions} from './raster.js';
+export type {
+  FeatureReadOptions,
+  GpxLayerOptions,
+  KmlLayerOptions,
+  VectorLayerOptions,
+  VectorTileLayerOptions,
+  WktLayerOptions,
+} from './vector.js';
